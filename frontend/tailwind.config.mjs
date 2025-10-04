a6c9d7dac7e37989da3830aad0bc4d36bf615c12
excniesNIED <hxcn@cnies.org>
2025-10-04 14:04:31 +0800

    Refactor ScheduleCalendar component for improved layout and responsiveness
    - Removed the 'xs' screen size from Tailwind configuration to streamline responsive design.
    - Adjusted padding and text sizes in `ScheduleCalendar.vue` for better readability and consistency across different screen sizes.
    - Enhanced multi-line display for course names and detailed information, optimizing the user interface for various devices.

--- frontend/tailwind.config.mjs
+++ frontend/tailwind.config.mjs
@@ -5,9 +5,6 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
-      screens: {
-        'xs': '475px',
-      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
